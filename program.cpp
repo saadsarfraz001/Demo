cff2f4e4caa5a962e306203a7e84a5f6f6f84c69
Saad <saadsarfraz001@gmail.com>
2019-12-03 10:30:23 +0500

    Initial commit

---
+++ program.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int num1 = 10;
+	int num2 = 5;
+	cout << "Addition : " << num1 + num2 << endl;
+	cout << "Subtraction : " << num1 - num2 << endl;
+	cout << "Multiplication : " << num1 * num2 << endl;
+	cout << "Division : " << num1 / num2 << endl;
+	return 0;
+}
