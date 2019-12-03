f8a9ec24428f3b13502f42aa45d5ae6716b63df9
Saad <saadsarfraz001@gmail.com>
2019-12-03 10:33:46 +0500

    Branch

--- program.cpp
+++ program.cpp
@@ -5,7 +5,7 @@ int main() {
 	int num2 = 5;
 	cout << "Addition : " << num1 + num2 << endl;
 	cout << "Subtraction : " << num1 - num2 << endl;
-	cout << "Multiplication : " << num1 * num2 << endl;
+	cout << "Multiply : " << num1 * num2 << endl;
 	cout << "Division : " << num1 / num2 << endl;
 	return 0;
 }
